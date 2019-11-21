540845404315fed6625ff667b3db10861ad69ab5
ASiarohin <ASiarohin@slb.com>
2019-11-21 12:25:42 +0300

    feat: add-dt-form

--- src/app/template-driven-forms/signup-form/signup-form.component.ts
+++ src/app/template-driven-forms/signup-form/signup-form.component.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+import { UserModel } from "./../../models/user.model";
 
 @Component({
-  selector: 'app-signup-form',
-  templateUrl: './signup-form.component.html',
-  styleUrls: ['./signup-form.component.css']
+  selector: "app-signup-form",
+  templateUrl: "./signup-form.component.html",
+  styleUrls: ["./signup-form.component.css"]
 })
 export class SignupFormComponent implements OnInit {
+  countries: Array<string> = [
+    "Ukraine",
+    "Armenia",
+    "Belarus",
+    "Hungary",
+    "Kazakhstan",
+    "Poland",
+    "Russia"
+  ];
+  user: UserModel = new UserModel();
 
-  constructor() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSave(signupForm: NgForm) {
+    // Form model
+    console.log(signupForm.form);
+    // Form value
+    console.log(`Saved: ${JSON.stringify(signupForm.value)}`);
   }
-
-  ngOnInit() {
-
-  }
-
 }
-
-
